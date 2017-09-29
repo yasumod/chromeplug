be8edf76529a3f63e47bb53e1c1752de5093c700
yoshiyasu.takashi <yoshiyasu.takashi@donuts.ne.jp>
2017-09-29 17:00:28 +0900

    初回コミット

--- js/background.js
+++ js/background.js
@@ -3,13 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var i = 0;
+var workUrlList = [];
+var urlList = [];
+var nowUrl = location.href;
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    // フィックス
+    if(request.action == "fix"){
+        // フィックスさせてワーク領域を解放
+        urlList[request.url] = workUrlList[request.url];
+        workUrlList = [];
+    }
+    // 現状取得
+    if(request.action == "now"){
+        sendResponse({data: urlList[nowUrl]});
+        return;
+    }
+
+    // レスポンス返却
+    sendResponse({data: urlList[request.url]});
+});
+
+// 通信終了時の退避
 chrome.webRequest.onCompleted.addListener(
   function delay_onCompleted (details) {
-    console.log(details);
-    chrome.runtime.sendMessage({url: details.url,name: "test"}, function(response) {});
+    workUrlList[details.url] = details;
     return;
   },
   { urls: [ "*://*/*" ] },
   [ "responseHeaders" ]
 );
+chrome.tabs.onUpdated.addListener(function(tabid, inf){
+  if (inf.status === 'complete') {
+      console.log(inf);
+    chrome.pageAction.show(tabid);
+    chrome.pageAction.setIcon({tabId:tabid, path:'icon.png'});
+  }
+});
--- js/content.js
+++ js/content.js
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-//メッセージリスナー
-console.log("regist listern");
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    alert(request.url);
-     var response = {data: "ツェペリの魂！！"};
-    sendResponse(response);
-});
+//
+//chrome.runtime.sendMessage({
+//    url: location.href,
+//    action : 'fix'
+//}, function(response) {
+//    //console.log("response info ["+ location.href + "] =========================== ")
+//    console.log(response);
+//});
